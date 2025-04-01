131d7592823f5a9d4e2e4a8b0605aa00efa1bd38
Illia Stetsiurenko <110241539+Stetsiurenko-Illia@users.noreply.github.com>
2025-04-01 23:40:00 +0300

    Add data sharing & online users tracking

--- todo_project/asgi.py
+++ todo_project/asgi.py
@@ -1,32 +1,20 @@
 import os
 from django.core.asgi import get_asgi_application
-from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 # Встановлюємо DJANGO_SETTINGS_MODULE і ініціалізуємо налаштування
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 django_asgi_app = get_asgi_application()
 
 # Імпортуємо після ініціалізації налаштувань
+import api.routing
 from api.middleware import JWTAuthMiddleware
-from api import consumers
 
-# Розділяємо маршрути для різних типів автентифікації
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": ProtocolTypeRouter({
-        # Для /ws/tasks/ використовуємо JWTAuthMiddleware
-        "tasks": JWTAuthMiddleware(
-            URLRouter([
-                path('ws/tasks/', consumers.TaskConsumer.as_asgi()),
-            ])
-        ),
-        # Для /ws/online-users/ використовуємо AuthMiddlewareStack (сесійна автентифікація)
-        "online-users": AuthMiddlewareStack(
-            URLRouter([
-                path('ws/online-users/', consumers.OnlineUsersConsumer.as_asgi()),
-            ])
-        ),
-    }),
+    "websocket": JWTAuthMiddleware(
+        URLRouter(
+            api.routing.websocket_urlpatterns
+        )
+    ),
 })
